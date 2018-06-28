ee73a13e7add1b9ffa9f2160dbf1bf228b1e3923
Dylan Weaver <dweaver@umd.edu>
2018-06-28 09:44:52 -0400

    fixed file format

--- MIPS Code/Loops-Basic-Addition.s
+++ MIPS Code/Loops-Basic-Addition.s
@@ -9,11 +9,11 @@
 		lw $t1 y
 		lw $t2 sum
 	
-		li $v0 5 # scanf("%d", &x);
+		li $v0 5 		# scanf("%d", &x);
 		syscall
 		move $t0 $v0
 		
-		li $v0 5 # scanf("%d", &y);
+		li $v0 5 		# scanf("%d", &y);
 		syscall
 		move $t1 $v0
 		
@@ -21,7 +21,7 @@
 		sw $t0 y
 		
 	loop:
-		bgt $t0 $t1 end_loop # if (x > y) goto end_loop
+		bgt $t0 $t1 end_loop 	# if (x > y) goto end_loop
 		
 		add $t2 $t2 $t0
 		add $t0 $t0 1
@@ -33,13 +33,13 @@
 		sw $t1 y
 		sw $t2 sum
 	
-		li $v0 1 # printf("%d", sum);
+		li $v0 1 		# printf("%d", sum);
 		move $a0 $t2
 		syscall
 		
-		li $v0 12 # printf("\n");
+		li $v0 12 		# printf("\n");
 		li $a0 '\n'
 		syscall
 		
-		li $v0 10 # return 0;
+		li $v0 10 		# return 0;
 		syscall
